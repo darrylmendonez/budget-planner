a938ff30de7e2c1221bf129b01184caa755e7646
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-04-12 11:46:14 -0400

    drawing enter selection using arcPath

--- js/graph.js
+++ js/graph.js
@@ -13,14 +13,53 @@ const pie = d3.pie()
   .sort(null)
   .value(d => d.cost);
 
-const angles = pie([
-  { name: 'rent', cost: 500 },
-  { name: 'bills', cost: 300 },
-  { name: 'gaming', cost:  200 },
-]);
-
 const arcPath = d3.arc()
   .outerRadius(dims.radius)
   .innerRadius(dims.radius / 2);
 
-console.log(arcPath(angles[0]))
+// update function
+const update = (data) => {
+  
+  // join enhanced (pie) data to path elements
+  const paths = graph.selectAll('path')
+    .data(pie(data));
+
+  paths.enter()
+    .append('path')
+      .attr('class', 'arc')
+      .attr('d', arcPath)
+      .attr('stroke', '#fff')
+      .attr('stroke-width', 3)
+
+}
+
+// data array and firestore
+var data = [];
+
+db.collection('expenses').onSnapshot(res => {
+
+  res.docChanges().forEach(change => {
+
+    const doc = { ...change.doc.data(), id: change.doc.id };
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        console.log('No changes made to data');
+        break;
+    }
+
+  });
+
+  update(data)
+
+})

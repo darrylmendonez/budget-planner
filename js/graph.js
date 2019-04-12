f9e2c9d1f031f2a70ec707f13049bdfdafad1073
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-04-12 13:11:08 -0400

    added click event to each slice that will delete that data item

--- js/graph.js
+++ js/graph.js
@@ -67,6 +67,7 @@ const update = (data) => {
   graph.selectAll('path')
     .on('mouseover', handleMouseOver)
     .on('mouseout', handleMouseOut)
+    .on('click', handleClick)
 }
 
 // data array and firestore
@@ -144,4 +145,9 @@ const handleMouseOut = (d, i, n) => {
   d3.select(n[i])
     .transition('changeSliceFill').duration(300)
       .attr('fill', color(d.data.name))
+}
+
+const handleClick = (d) => {
+  const id = d.data.id;
+  db.collection('expenses').doc(id).delete();
 }
